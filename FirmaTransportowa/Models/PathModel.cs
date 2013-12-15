0d4c8b4a9d1c67e162e62fb0de85f2300a22cd8f
radk0s <radk0slaw@gmail.com>
2013-12-15 21:16:24 +0100

    fix

---
+++ FirmaTransportowa/Models/PathModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirmaTransportowa.Models
+{
+    public class PathModel
+    {
+        public int iters { get; set; }
+        public int fireflies { get; set; }
+        public List<List<int>> matrix { get; set; }
+    }
+}
